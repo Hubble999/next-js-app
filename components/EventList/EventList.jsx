43e429ebb34737ce1a2566dc9b5b7b8522428f70
karen9999 <karen.dev999@gmail.com>
2021-05-04 20:31:35 +0300

    created EventList component

---
+++ components/EventList/EventList.jsx
@@ -0,0 +1,21 @@
+import EventItem from '../EventItem/EventItem';
+import classes from './EventList.module.scss';
+
+const EventList = ({ items }) => {
+  return (
+    <ul className={classes.list}>
+      {items.map(({ id, title, location, date, image }) => (
+        <EventItem
+          key={id}
+          id={id}
+          title={title}
+          location={location}
+          date={date}
+          image={image}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default EventList;
