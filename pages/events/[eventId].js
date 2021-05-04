2481b0987cad73bf0c3f1f77f3d520cb9c8a843d
karen9999 <karen.dev999@gmail.com>
2021-05-04 20:44:07 +0300

    created EventDetailPage

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,11 +1,34 @@
-import React from 'react';
+import { useRouter } from 'next/router';
 
-const Event = () => {
+import events from '../../data.js';
+import EventSummary from '../../components/EventSummary/EventSummary';
+import EventLogistics from '../../components/EventLogistics/EventLogistics';
+import EventContent from '../../components/EventContent/EventContent';
+
+const EventDetailPage = () => {
+  const router = useRouter();
+
+  const eventId = router.query.eventId;
+  const event = events.filter((event) => event.id === eventId)[0];
+
+  if (!event) {
+    return <p>No event found!</p>;
+  }
+
   return (
-    <div>
-      <h1>Event ID</h1>
-    </div>
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
   );
 };
 
-export default Event;
+export default EventDetailPage;
