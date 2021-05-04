08b8a014ee1aacfe3be2d80cea08aa4507942960
karen9999 <karen.dev999@gmail.com>
2021-05-04 20:29:57 +0300

    wrapping components in MainHeader

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import MainHeader from '../components/MainHeader/MainHeader';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MainHeader>
+      <Component {...pageProps} />
+    </MainHeader>
+  );
 }
 
-export default MyApp
+export default MyApp;
