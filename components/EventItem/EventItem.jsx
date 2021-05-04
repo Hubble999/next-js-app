0bc8e9d6adf6fd3c8a66b39c855ad7c6956ccfd0
karen9999 <karen.dev999@gmail.com>
2021-05-04 20:30:55 +0300

    created EventItem component

---
+++ components/EventItem/EventItem.jsx
@@ -0,0 +1,43 @@
+import Button from '../Button/Button';
+import DateIcon from '../DateIcon/DateIcon';
+import AddressIcon from '../AddressIcon/AddressIcon';
+import ArrowRightIcon from '../ArrowRightIcon/ArrowRightIcon';
+import classes from './EventItem.module.scss';
+import { makeReadableDate } from '../../utils.js';
+
+function EventItem(props) {
+  const { title, image, date, location, id } = props;
+
+  const readableDate = makeReadableDate(date);
+  const formattedAddress = location.replace(', ', '\n');
+  const link = `/events/${id}`;
+
+  return (
+    <li className={classes.item}>
+      <img src={'/' + image} alt={title} />
+      <div className={classes.content}>
+        <div className={classes.summary}>
+          <h2>{title}</h2>
+          <div className={classes.date}>
+            <DateIcon />
+            <time>{readableDate}</time>
+          </div>
+          <div className={classes.address}>
+            <AddressIcon />
+            <address>{formattedAddress}</address>
+          </div>
+        </div>
+        <div className={classes.actions}>
+          <Button link={link}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
+        </div>
+      </div>
+    </li>
+  );
+}
+
+export default EventItem;
