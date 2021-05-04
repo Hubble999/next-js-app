6b9e4270a280a36fe17adee8ef1f5c5294ff68c1
karen9999 <karen.dev999@gmail.com>
2021-05-04 20:32:42 +0300

    created Button

---
+++ components/Button/Button.jsx
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+
+import classes from './Button.module.scss';
+
+const Button = (props) => {
+  if (props.link) {
+    return (
+      <Link className={classes.btn} href={props.link}>
+        <a className={classes.btn}>{props.children}</a>
+      </Link>
+    );
+  }
+
+  return (
+    <button className={classes.btn} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+}
+
+export default Button;
