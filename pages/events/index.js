cd7892c691e0f2df4c1555ba5106bdef301b1938
karen9999 <karen.dev999@gmail.com>
2021-05-04 20:42:38 +0300

    created events page /

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useRouter } from 'next/router';
+import EventList from '../../components/EventList/EventList';
+import EventSearch from '../../components/EventSearch/EventSearch';
+import events from '../../data.js';
 
 const Events = () => {
-  return <div>
-    <h1>Events</h1>
-  </div>
+  const router = useRouter();
+
+  const findEventsHandler = (year, month) => {
+    router.push(`/events/${year}/${month}`);
+  };
+
+  return (
+    <>
+      <EventSearch onSearch={findEventsHandler} />
+      <EventList items={events} />
+    </>
+  );
 };
 
 export default Events;
