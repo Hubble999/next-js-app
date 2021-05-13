cfb86437d42779bc71dee1ac36f509320db2b8c9
karen9999 <karen.dev999@gmail.com>
2021-05-13 17:16:05 +0300

    created helper function for fetch data

--- utils.js
+++ utils.js
@@ -1,5 +1,28 @@
-export const makeReadableDate = (date) => new Date(date).toLocaleDateString('en-US', {
+export const makeReadableDate = (date) =>
+  new Date(date).toLocaleDateString('en-US', {
     day: 'numeric',
     month: 'long',
     year: 'numeric',
   });
+
+export const getEvents = async () => {
+  const URL = 'https://next-js-f6bc0-default-rtdb.europe-west1.firebasedatabase.app/events.json';
+  const res = await fetch(URL);
+  const data = await res.json();
+  const events = Object.keys(data).map((key) => ({ id: key, ...data[key] }));
+  return events;
+};
+
+export const getFilteredEvents = async (year, month) => {
+  const events = await getEvents();
+  return events.filter(({ date }) => {
+    const currentYear = new Date(date).getFullYear();
+    const currentMonth = new Date(date).getMonth() + 1;
+    return currentYear === Number(year) && currentMonth === Number(month);
+  });
+};
+
+export const getEventById = async (eventId) => {
+  const events = await getEvents();
+  return events.find(({ id }) => id === eventId);
+};
