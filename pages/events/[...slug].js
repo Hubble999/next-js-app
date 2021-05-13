4882c4e0d091153bdd5f47cc5e6c5344cb1bad6b
karen9999 <karen.dev999@gmail.com>
2021-05-13 17:19:53 +0300

    add server side rendering

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,11 +1,36 @@
 import React from 'react';
+import { useRouter } from 'next/router';
+import events from '../../data';
+import EventList from '../../components/EventList/EventList';
+import Button from '../../components/Button/Button';
+import { getFilteredEvents } from '../../utils.js';
 
-const FilteredEventsPage = () => {
-  return (
+const FilteredEventsPage = ({ filteredEvents }) => {
+  console.log(filteredEvents)
+  if (!filteredEvents) {
+    return <h2>Loading...</h2>;
+  }
+
+  return filteredEvents.length === 0 ? (
     <div>
-      <h1>Event Deatail</h1>
+      <h2>No events found</h2>
+      <Button link={'/events'}>Go back</Button>
+    </div>
+  ) : (
+    <div>
+      <EventList items={filteredEvents} />
     </div>
   );
 };
+
+export async function getServerSideProps({ params }) {
+  const [year, month] = params.slug;
+  const filteredEvents = await getFilteredEvents(year, month);
+  return {
+    props: {
+      filteredEvents,
+    },
+  };
+}
 
 export default FilteredEventsPage;
