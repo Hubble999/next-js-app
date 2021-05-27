ef7ca74992aba5098816c8525ea655b42cd33f1f
karen9999 <karen.dev999@gmail.com>
2021-05-27 07:39:41 +0300

    fix margin for EventList component

--- next.config.js
+++ next.config.js
@@ -1,23 +1,10 @@
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
-
-module.exports = (phase) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return {
-      env: {
-        mongodb_username: 'mark',
-        mongodb_password: 'XBIMy29EXnpacMJC',
-        mongodb_clustername: 'cluster0',
-        mongodb_database: 'events-dev',
-      },
-    };
-  }
-
+module.exports = () => {
   return {
     env: {
       mongodb_username: 'mark',
       mongodb_password: 'XBIMy29EXnpacMJC',
       mongodb_clustername: 'cluster0',
-      mongodb_database: 'events',
-    },
+      mongodb_database: 'events'
+    }
   };
 };
